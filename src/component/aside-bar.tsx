d663adc94f496fb8a02b390858e6ca64f5488b3f
ss7m <Sean.Y.Shu@newegg.com>
2019-07-30 17:10:17 +0800

    add single page rooter

--- src/component/aside-bar.tsx
+++ src/component/aside-bar.tsx
@@ -1,31 +1,44 @@
 import * as React from "react";
 import { IoIosSearch } from "react-icons/io";
+import hashFilter from "../../tools/hashFilter"
 interface Iprops {
   title: string;
   subTitleList: Array<{ name: string; hash: string }>;
 }
 interface Istate {
   subTitle: Array<{ name: string; hash: string }>;
+  hash: string;
 }
 export default class AsideBar extends React.Component<Iprops, Istate> {
   private input: any;
   constructor(props: Iprops) {
     super(props);
     this.state = {
-      subTitle: this.props.subTitleList
+      subTitle: this.props.subTitleList,
+      hash: ""
     };
   }
-  handleOnChange = () => {
+  private hashHander = () => {
+    this.setState({ hash: window.location.hash.substr(1) });
+  };
+  private handleOnChange = () => {
     let subTitle = this.props.subTitleList.filter(
       item => item.name.indexOf(this.input.value) !== -1
     );
     this.setState({
-      subTitle:subTitle,
+      subTitle: subTitle
     });
   };
+  componentDidMount() {
+    window.addEventListener("hashchange", this.hashHander);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("hashchange", this.hashHander);
+  }
   render() {
     const { title } = this.props;
     const { subTitle } = this.state;
+    const child = hashFilter(this.state.hash);
     return (
       <React.Fragment>
         <nav className="nav-container">
@@ -43,11 +56,12 @@ export default class AsideBar extends React.Component<Iprops, Istate> {
           <ul className="nav-list">
             {subTitle.map(item => (
               <li key={item.hash} className="nav-list-cld">
-                <a href="#">{item.name}</a>
+                <a href={`#${item.hash}`}>{item.name}</a>
               </li>
             ))}
           </ul>
         </nav>
+        {child}
       </React.Fragment>
     );
   }
---
+++ tools/hashFilter.ts
@@ -0,0 +1,10 @@
+export default function hashFilter(hash:string) {
+    switch(hash) {
+        case '':
+        return 'index'
+        case 'test1':
+        return 'test1'
+        case 'test2':
+        return 'test2'
+    }
+}
