2ca46717b3a7039afe852297b69ebe23ae3d474b
ss1x <Sean.Y.Shu@newegg.com>
2019-09-28 11:17:18 +0800

    feat: cascader stage:1

--- src/index/component/cascader.tsx
+++ src/index/component/cascader.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
-
+import '../../../styleSheet/cascader.css'
 export default class Cascader extends React.Component {
     render() {
-        return 'Cascader';
+        return (
+        <div className="cascader-wapper">
+            <h3>Cascader</h3>
+            <input type="text" placeholder="Please select" className="cascader-input"/>
+        </div>
+        );
     }
 }
